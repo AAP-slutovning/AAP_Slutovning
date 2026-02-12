623e97ff33980d7ee35643fb84e9c0289548e104
lenajohnsson <lenajohnsson85@gmail.com>
2026-02-12 10:44:51 +0100

    implementerat felmeddelande om försök till att klarmarkering försöks på en redan klarmarkerad uppgift

--- AAP_slutovning/Program.cs
+++ AAP_slutovning/Program.cs
@@ -97,6 +97,11 @@
                 return;
             }
 
+            if (task.IsCompleted == true)
+            {
+                Console.WriteLine($"Uppgift {id} är redan markerad som klar.");
+            }
+
             task.IsCompleted = true;
             Console.WriteLine($"Uppgift {id} är markerad som klar.");
         }
